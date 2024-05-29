2a5fd3128f92cedae2d5806487cd4cf885b70f31
kevinjeremia <jungzstrumida@gmail.com>
2024-05-30 00:05:40 +0700

    feat: implement splash screen
    also add onboarding and login fragment placeholder

--- app/src/main/java/com/bangkit/android/dermatify/ui/MainActivity.kt
+++ app/src/main/java/com/bangkit/android/dermatify/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bangkit.android.dermatify
+package com.bangkit.android.dermatify.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkit.android.dermatify.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/bangkit/android/dermatify/ui/login/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.bangkit.android.dermatify.ui.login
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bangkit.android.dermatify.R
+import com.bangkit.android.dermatify.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireActivity().window.statusBarColor = activity?.getColor(R.color.primary_blue) ?: Color.TRANSPARENT
+        requireActivity().window.navigationBarColor = activity?.getColor(R.color.primary_blue) ?: Color.TRANSPARENT
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+
+}
