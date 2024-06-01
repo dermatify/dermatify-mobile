9e5bc83bab17357345c3aca05bedc50593dd6ec9
jasmineazzahra13 <zahrabrilliant1323@gmail.com>
2024-06-01 17:19:54 +0700

    feat: implement login & register

--- app/src/main/java/com/bangkit/android/dermatify/ui/login/LoginFragment.kt
+++ app/src/main/java/com/bangkit/android/dermatify/ui/login/LoginFragment.kt
@@ -6,13 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.ViewModelProvider
 import com.bangkit.android.dermatify.R
 import com.bangkit.android.dermatify.databinding.FragmentLoginBinding
+import com.google.android.material.snackbar.Snackbar
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private lateinit var viewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +32,34 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        binding.btnLogin.setOnClickListener {
+            val email = binding.edtEmailLogin.text.toString()
+            val password = binding.edtPasswordLogin.text.toString()
+            viewModel.login(email, password)
+        }
+        viewModel.loginSuccess.observe(viewLifecycleOwner, {isSuccess ->
+            if (isSuccess) {
 
+            } else {
+                Snackbar.make(view, "Login failed. Invalid email or password", Snackbar.LENGTH_SHORT).show()
+            }
+        })
+        binding.edtPasswordLogin.addTextChangedListener {
+            val password = it.toString()
+            if (password.length < 8) {
+                binding.edtPasswordLogin.error = "Password must be at least 8 characters"
+            } else {
+                binding.edtPasswordLogin.error = null
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding  = null
+    }
 
 }
