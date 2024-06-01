f1605650ddc669912ea92ee377cfe4645232cc95
jasmineazzahra13 <zahrabrilliant1323@gmail.com>
2024-06-01 17:19:54 +0700

    feat: implement login & register

--- app/src/main/java/com/bangkit/android/dermatify/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/bangkit/android/dermatify/ui/login/LoginViewModel.kt
@@ -1,7 +1,15 @@
 package com.bangkit.android.dermatify.ui.login
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    val loginSuccess: MutableLiveData<Boolean> =
+        MutableLiveData()
+
+    fun login(email: String, password: String) {
+        val isLoggedIn = email.isNotBlank() && password.isNotBlank()
+        loginSuccess.value = isLoggedIn
+    }
 }
