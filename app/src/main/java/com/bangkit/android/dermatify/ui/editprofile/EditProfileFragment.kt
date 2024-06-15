728fb996ce12c77511e942194e780df4252de0f4
kevinjeremia <jungzstrumida@gmail.com>
2024-06-16 04:12:43 +0700

    feat: add gallery photo picker in edit profile

--- app/src/main/java/com/bangkit/android/dermatify/ui/editprofile/EditProfileFragment.kt
+++ app/src/main/java/com/bangkit/android/dermatify/ui/editprofile/EditProfileFragment.kt
@@ -1,21 +1,57 @@
 package com.bangkit.android.dermatify.ui.editprofile
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bangkit.android.dermatify.R
 import com.bangkit.android.dermatify.databinding.FragmentEditProfileBinding
+import com.bangkit.android.dermatify.util.gone
+import com.bangkit.android.dermatify.util.invisible
+import com.bangkit.android.dermatify.util.showSnackbar
 
 class EditProfileFragment : Fragment() {
     private var _binding: FragmentEditProfileBinding? = null
     private val binding get() = _binding!!
 
     private val navigationArgs: EditProfileFragmentArgs by navArgs()
+
+    private val userEmail = navigationArgs.email
+    private var userProfilePic: Uri? = null
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                binding.root.showSnackbar(getString(R.string.camera_permission_granted), "success", binding.btnSave)
+            } else {
+                binding.root.showSnackbar(getString(R.string.camera_permission_denied), "error", binding.btnSave)
+            }
+        }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            userProfilePic = uri
+            binding.apply {
+                ivProfile.invisible()
+                ivProfilePicPh.showImage()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,16 +73,40 @@ class EditProfileFragment : Fragment() {
     }
 
     private fun initUI() {
+        if (!checkPermission(CAMERA_PERMISSION)) {
+            requestPermissionLauncher.launch(CAMERA_PERMISSION)
+        }
+
         binding.apply {
-            tvEmail.text = navigationArgs.email
+            tvEmail.text = userEmail
 
             topbar.setNavigationOnClickListener {
                 findNavController().popBackStack()
             }
 
             btnChangeProfilePic.setOnClickListener {
-
+                startGallery()
             }
         }
+    }
+
+    private fun ImageView.showImage() {
+        userProfilePic?.let {
+            this.setImageURI(it)
+        }
+    }
+
+    private fun checkPermission(permission: String): Boolean =
+        ContextCompat.checkSelfPermission(
+            requireContext(),
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    companion object {
+        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
     }
 }
