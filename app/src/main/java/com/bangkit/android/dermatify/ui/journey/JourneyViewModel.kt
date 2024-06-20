2dd94df37144f71b130e0dd9bf6400497d223ca4
jasmineazzahra13 <zahrabrilliant1323@gmail.com>
2024-06-20 18:39:03 +0700

    feat: room db

--- app/src/main/java/com/bangkit/android/dermatify/ui/journey/JourneyViewModel.kt
+++ app/src/main/java/com/bangkit/android/dermatify/ui/journey/JourneyViewModel.kt
@@ -1,7 +1,10 @@
 package com.bangkit.android.dermatify.ui.journey
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bangkit.android.dermatify.data.local.entity.ScansEntity
+import kotlinx.coroutines.launch
 
 class JourneyViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
